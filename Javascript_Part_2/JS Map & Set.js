0512037f826453d97d5d8ad238f65c42474f2195
shubham20071997 <mauryashubham204@gmail.com>
2022-08-29 01:00:06 +0530

    Javascript Learnings

--- Javascript_Part_2/JS Map & Set.js
+++ Javascript_Part_2/JS Map & Set.js
@@ -1,12 +1,28 @@
 // JS Sets
-const mySet = new Set();
-mySet.add("this");
-mySet.add("that");
-mySet.add("Your name");
-mySet.add(56);
-console.log(mySet);
+// const mySet = new Set();
+// mySet.add("this");
+// mySet.add("that");
+// mySet.add("Your name");
+// mySet.add(56);
+// console.log(mySet);
 
 // Set Constructor
 // let mySet2 = new Set ([1, 45, "this", {a:4, b:8}]
 // )
 // console.log(mySet2);
+// console.log(mySet.size);
+// console.log(mySet.has(56));
+// mySet.delete ("that");
+// console.log(mySet);
+
+// JS Maps
+// const myMap = new Map ();
+// console.log (myMap);
+// const key1 = "my str", key2 = {};
+// myMap.set (key1);
+// myMap.set (key2);
+
+
+
+
+
---
+++ Javascript_Part_2/JS regular expression.js
@@ -0,0 +1,13 @@
+let reg = /shubham/; // regular expression literal in js
+// reg = /shubham/g; // g means global
+// reg = /shubham/i; // i means case insensitive
+
+let s = "This is great code comprising shubham along with Manvi & Shwet";
+// Functions to match expressions
+// 1. exec() - This function will return an array for match or null for no match
+// let result = reg.exec(s);
+console.log(result);
+result = reg.exec(s);
+
+
+
